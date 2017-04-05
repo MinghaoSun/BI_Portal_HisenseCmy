bf7fd05606a4e22796add54903aa1c261b930c21
MinghaoSun <mhaosun@outlook.com>
2017-04-05 13:57:20 +0800

    新加页面

---
+++ source/web/page_new/report/view/abcddddee/indexController.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+infopowerWebApp.controller('abcddddeeindexCtrl', ['$scope', '$http', '$controller', '$routeParams','$state',
+    function ($scope, $http, $controller, $routeParams,$state) {
+	  alert("ddda");
+    }
+]);
